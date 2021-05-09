8442ff417581b17ca2e21482a434d474a1de0d66
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-05-09 13:00:13 -0700

    index.js: added 'myFavMovies' (my top 10 movies for json). app now listening to port 8080. localhost:8080/movies pulls up myFavMovies.json

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const app = express();
+
+let myFavMovies = [
+  {
+    title: `Grandma's Boy`,
+    director: 'Nicholaus Goosen',
+  },
+  {
+    title: 'Fight Club',
+    director: 'David Fincher',
+  },
+  {
+    title: 'The Hateful Eight',
+    director: 'Quentin Tarantino',
+  },
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+  res.json(myFavMovies);
+});
+
+//  listen for requests
+app.listen(8080, () => {
+  console.log('movies-api is currently listening to port 8080');
+});
