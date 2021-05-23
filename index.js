4e20ae638ed29d24fdcfb41acf6cd09df224b405
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-05-23 15:56:17 -0700

    index.js: imported/required passport and passport.js

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express'),
   morgan = require('morgan'),
   mongoose = require('mongoose'),
   Models = require('./models.js'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  passport = require('passport');
+
+require('./passport');
 
 const app = express();
 app.use(bodyParser.json());
 const Movies = Models.Movie;
 const Users = Models.User;
+
+let auth = require('./auth')(app);
 
 mongoose.connect('mongodb://localhost:27017/myFlixDb', {
   useNewUrlParser: true,
