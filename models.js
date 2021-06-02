06f0cb6233fb47337c48f27550c2db99f9a94336
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-06-02 14:11:17 -0700

    testing & troubleshooting

--- models.js
+++ models.js
@@ -20,10 +20,10 @@ let movieSchema = mongoose.Schema({
 
 //  defines the model for users to match pre-existing data in mongodb
 let userSchema = mongoose.Schema({
-  Name: { type: String.toLowerCase(), required: false },
-  Username: { type: String.toLowerCase(), required: true },
-  Password: { type: String.toLowerCase(), required: true },
-  Email: { type: String.toLowerCase(), required: true },
+  Name: { type: String, lowercase: true, required: false },
+  Username: { type: String, lowercase: true, required: true },
+  Password: { type: String, lowercase: true, required: true },
+  Email: { type: String, lowercase: true, required: true },
   Birthdate: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
