07f0566e37f469da0e8f9ae8fa3a40e017047af9
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-06-02 14:56:39 -0700

    troubleshooting

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ let movieSchema = mongoose.Schema({
 //  defines the model for users to match pre-existing data in mongodb
 let userSchema = mongoose.Schema({
   Name: { type: String, required: false, lowercase: true },
-  Username: { type: String, required: true },
+  Username: { type: String, required: true, lowercase: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birthdate: Date,
